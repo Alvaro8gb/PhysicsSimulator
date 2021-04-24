34f9fa77e47d276b5915f8e413d8e48fe46bbae5
vmoren04 <vmoren04@ucm.es>
2021-04-24 14:24:14 +0200

    Terminado BodiesTable y BodiesTableModel , faltaria probar más adelante

--- src/simulator/view/BodiesTable.java
+++ src/simulator/view/BodiesTable.java
@@ -4,22 +4,46 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 
 import javax.swing.BorderFactory;
+import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextField;
 import javax.swing.border.TitledBorder;
 
 import simulator.control.Controller;
+import simulator.model.NoForce;
+import simulator.model.PhysicsSimulator;
 
 public class BodiesTable extends JPanel {
 	
 	private static final long serialVersionUID = 1L;
-
+	//tabla
+	private JTable tabla;
+	//modelo de la tabla
+	private BodiesTableModel btm;
+	
 	BodiesTable(Controller ctrl) {
 	
 		setLayout(new BorderLayout());
 		setBorder(BorderFactory.createTitledBorder( BorderFactory.createLineBorder(Color.black, 2),"Bodies",TitledBorder.LEFT, TitledBorder.TOP));
-	
-	
-	
+		btm = new BodiesTableModel(ctrl);
+
+		init();
+		
 	
 	}
+	private void init() {
+		//tabla
+		tabla = new JTable(btm);
+		add(new JScrollPane(tabla));
+		
+	}
+	 public static void main(String args[]) {
+
+		  JFrame j = new JFrame();
+		  j.add( new BodiesTable(new Controller(new PhysicsSimulator(20,new NoForce()),null,null)));
+		  j.setVisible(true);
+		  j.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	   }
 }
--- src/simulator/view/BodiesTableModel.java
+++ src/simulator/view/BodiesTableModel.java
@@ -13,10 +13,12 @@ public class BodiesTableModel extends AbstractTableModel implements SimulatorObs
 // ...
 	private List<Body> _bodies;
 	private final int numberOfCols = 5;
+	private final String[] columnNames;
 	
 	BodiesTableModel(Controller ctrl) {
 	_bodies = new ArrayList<>();
 	ctrl.addObserver(this);
+	this.columnNames=new String[]{"Id","Mass","Position","Velocity","Force"};
 	}
 	@Override
 	public int getRowCount() {
@@ -29,21 +31,31 @@ public class BodiesTableModel extends AbstractTableModel implements SimulatorObs
 	@Override
 	public String getColumnName(int column) {
 	 
-		switch(column) {
-		
-		case 0 : return "id";
-		case 1 : return "Mass";
-		case 2 : return "Position";
-		case 3 : return "Velocity";
-		case 4 : return "Force";
-		
-		}
-		
-		return null;
+		return columnNames[column];
 	}
 	@Override
 	public Object getValueAt(int rowIndex, int columnIndex) {
-	 return null;
+			Object value = null;
+			Body b = _bodies.get(rowIndex);
+			switch(columnIndex) {
+			case 0:
+				value = b.getId();
+				break;
+			case 1:
+				value = b.getMass();;
+				break;
+			case 2:
+				value = b.getPosition();
+				break;
+			case 3:
+				value = b.getVelocity();
+				break;
+			case 4:
+				value = b.getForce();
+				break;
+
+			}
+		return value;
 	}
 	
 	
@@ -52,22 +64,26 @@ public class BodiesTableModel extends AbstractTableModel implements SimulatorObs
 	@Override
 	public void onRegister(List<Body> bodies, double time, double dt, String fLawsDesc) {
 		// TODO Auto-generated method stub
-		
+		this._bodies = bodies;
+		fireTableStructureChanged();
 	}
 	@Override
 	public void onReset(List<Body> bodies, double time, double dt, String fLawsDesc) {
 		// TODO Auto-generated method stub
-		
+		this._bodies = bodies;
+		fireTableStructureChanged();
 	}
 	@Override
 	public void onBodyAdded(List<Body> bodies, Body b) {
 		// TODO Auto-generated method stub
-		
+		this._bodies = bodies;
+		fireTableStructureChanged();
 	}
 	@Override
 	public void onAdvance(List<Body> bodies, double time) {
 		// TODO Auto-generated method stub
-		
+		this._bodies = bodies;
+		fireTableStructureChanged();
 	}
 	@Override
 	public void onDeltaTimeChanged(double dt) {
