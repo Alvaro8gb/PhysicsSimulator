43e17c196a27aeb4a1d10a61eaaa619055bba6d2
vmoren04 <vmoren04@ucm.es>
2021-04-30 12:39:54 +0200

    Mejora ForceLawWindow

--- src/simulator/view/ForceLawWindow.java
+++ src/simulator/view/ForceLawWindow.java
@@ -26,8 +26,8 @@ public class ForceLawWindow extends JFrame{
 		j.setVisible(true);
 		j.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		j.setTitle("Force Laws Selection");
-		j.pack();
 		initGIU(j);
+		j.pack();
 	}
 	private void initGIU(JFrame j) {
 		//mensaje arriba
@@ -35,24 +35,31 @@ public class ForceLawWindow extends JFrame{
 		 j.add(help,BorderLayout.NORTH);
 		 //MEDIO
 		 JPanel mid = new JPanel();
-		 mid.setLayout(new BoxLayout(mid,BoxLayout.Y_AXIS));
+		 mid.setLayout(new BorderLayout());
 		 mid.setVisible(true);
+		 //parte alta del medio
+		 //parte baja del medio
 		//aparezcan las force law en la ComboBox
-		JComboBox list = new JComboBox<String>();
-		options = _ctrl.getForceLawsInfo();
-		for(JSONObject x : options) {
-			list.addItem(x.getString("desc"));
-		}
-		//eleccion fuerza
-		
-		/*list.addActionListener(new ActionListener() {@Override public void actionPerformed(ActionEvent e) { 
-			if(list.getSelectedItem().equals("Newton’s law of universal gravitation")) _ctrl.setForceLaws(info);
-			else if(list.getSelectedItem().equals("No force"))_ctrl.setForceLaws(info);
-			else if(list.getSelectedItem().equals("Moving towards a fixed point"))_ctrl.setForceLaws(info);
-			else if(list.getSelectedItem().equals("Circular aleatory force"))_ctrl.setForceLaws(info);
-		}
-		});*/
-		mid.add(list);
+		 JPanel middown = new JPanel();
+		 middown.setLayout(new FlowLayout(FlowLayout.CENTER,5,5));
+		 	JLabel info = new JLabel("Force Laws: ");
+			JComboBox list = new JComboBox<String>();
+			options = _ctrl.getForceLawsInfo();
+			for(JSONObject x : options) {
+				list.addItem(x.getString("desc"));
+			}
+			//eleccion fuerza
+			
+			/*list.addActionListener(new ActionListener() {@Override public void actionPerformed(ActionEvent e) { 
+				if(list.getSelectedItem().equals("Newton’s law of universal gravitation")) _ctrl.setForceLaws(info);
+				else if(list.getSelectedItem().equals("No force"))_ctrl.setForceLaws(info);
+				else if(list.getSelectedItem().equals("Moving towards a fixed point"))_ctrl.setForceLaws(info);
+				else if(list.getSelectedItem().equals("Circular aleatory force"))_ctrl.setForceLaws(info);
+			}
+			});*/
+			middown.add(info);
+			middown.add(list);
+		mid.add(middown,BorderLayout.SOUTH);
 		j.add(mid,BorderLayout.CENTER);
 		//FINAL
 		JPanel end = new JPanel();
