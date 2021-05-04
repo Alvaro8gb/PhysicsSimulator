e664487f1f898da2a5d0c478777cbe6bec880c9e
vmoren04 <vmoren04@ucm.es>
2021-05-04 11:38:18 +0200

    avance forcelawsWindow

--- src/simulator/view/ParametersTableModel.java
+++ src/simulator/view/ParametersTableModel.java
@@ -1,11 +1,16 @@
 package simulator.view;
 
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Set;
 
 import javax.swing.table.AbstractTableModel;
 
 import org.json.JSONObject;
+
+import simulator.model.Body;
 
 
 public class ParametersTableModel extends AbstractTableModel {
@@ -15,14 +20,16 @@ public class ParametersTableModel extends AbstractTableModel {
 	private JSONObject _lawInfo;
 	private final int numberOfCols = 3;
 	private final String[] columnNames;
+	private final List<String> keys;
 	
 	ParametersTableModel(JSONObject lawInfo) {
 		_lawInfo = lawInfo;
 	   columnNames = new String[]{"Key","Value","Description"};
+	   keys = getKeys();
 	}
 	@Override
 	public int getRowCount() {
-	 return _lawInfo.length();
+	 return _lawInfo.getJSONObject("data").length();
 	}
 	@Override
 	public int getColumnCount() {
@@ -34,7 +41,29 @@ public class ParametersTableModel extends AbstractTableModel {
 	}
 	@Override
 	public Object getValueAt(int rowIndex, int columnIndex) {
-		return "hola";
+		Object value = null;
+		String s = keys.get(rowIndex);
+		switch(columnIndex) {
+		case 0:
+			value = s;
+			break;
+		case 1:
+			value = "hola";
+			break;
+		case 2:
+			value = _lawInfo.getJSONObject("data").getString(s);
+			break;
+
+		}
+	return value;
+	}
+	private List<String> getKeys() {
+		List<String> x = new ArrayList<>();
+		for( Iterator it = _lawInfo.getJSONObject("data").keySet().iterator(); it.hasNext();) { 
+		    String s = (String)it.next();
+		    x.add(s);
+		  }
+		return x;
 	}
 
 }
